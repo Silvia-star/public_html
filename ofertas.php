f4d2190eb4e87bdfe6141f1f735531f29bf4d91b
Silvi-star <you@example.com>
2020-04-03 12:35:57 +0200

    añadoPublicidad

---
+++ ofertas.php
@@ -0,0 +1,33 @@
+<?php include "partials/head.php" ?>
+<?php include "partials/nav.php" ?>
+
+
+    <section class="hero-wrap hero-wrap-2" style="background-image: url('images/bg_3.jpg');" data-stellar-background-ratio="0.5">
+      <div class="overlay"></div>
+      <div class="container">
+        <div class="row no-gutters slider-text align-items-end justify-content-center">
+          <div class="col-md-9 ftco-animate text-center">
+            <h1 class="mb-2 bread">Ofertas</h1>
+            <p class="breadcrumbs"><span class="mr-2"><a href="index.php">Inicio <i class="ion-ios-arrow-forward"></i></a></span> <span>Ofertas</span></p>
+          </div>
+        </div>
+      </div>
+    </section>
+	
+
+	<section>
+		
+  <img src="bocadillo.jpg">
+
+	</section>
+  <section>
+  <p>Ejemplos de publicidad</p>  
+  <img src="publi1.jpg">
+  <img src="publi2.png">
+  </section>
+
+
+<!--?php include "partials/footer.php"; ?-->
+<?php include "partials/loader.php"; ?>
+<?php include "partials/scripts.php"; ?>
+<?php include "partials/end.php"; ?>
---
+++ partials/nav.php
@@ -0,0 +1,14 @@
+<nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
+	<div class="container">
+	  <a class="navbar-brand" href="index.php">Restaurante Palacer</a>
+	  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+	    <span class="oi oi-menu"></span>
+	  </button>
+
+	  <div class="collapse navbar-collapse" id="ftco-nav">
+	    <ul class="navbar-nav ml-auto">
+	      <li class="nav-item cta"><a href="ofertas.php" class="nav-link">Ofertas</a></li>
+	    </ul>
+	  </div>
+	</div>
+</nav>
